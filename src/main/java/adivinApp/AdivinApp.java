44f52f8e48b7406f38617c840829d6b63a4be9f5
JosemiBetancor <josemiguelbetancorvelazquez@hotmai.com>
2022-09-28 09:55:50 +0100

    Correccion

--- src/main/java/adivinApp/AdivinApp.java
+++ src/main/java/adivinApp/AdivinApp.java
@@ -2,16 +2,14 @@ package adivinApp;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.layout.HBox;
+
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -20,7 +18,7 @@ public class AdivinApp extends Application {
 	private TextField Textfield;
 	private Button ComprobarButton;
 	private VBox root;
-	private HBox buttons;
+
 	private Text enunciado;
 	private int solucion, intentos = 0;
 
